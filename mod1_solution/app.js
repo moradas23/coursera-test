1f82f9132b8444b70f97e54382321043e7cadfe2
Sergio Moradas <sergimoradas@gmail.com>
2016-11-12 18:01:55 +0100

    NewMD1

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -16,20 +16,40 @@ function LunchCheckController($scope){
 
    if(numItems == 1){
      var firstItem = items[0];
-     if(firstItem == "")
+     if(firstItem == ""){
        $scope.message= "Please enter data first";
+       $scope.empty();
+     }
      else {
        $scope.message= "Enjoy!";
+       $scope.enjoyOrTooMuch();
      }
    }
    else if (numItems<=3) {
      $scope.message= "Enjoy!";
+     $scope.enjoyOrTooMuch();
    }
    else if (numItems>3) {
       $scope.message= "Too much!";
+      $scope.enjoyOrTooMuch();
    }
+  }
 
- };
+  $scope.CustomStyle = {};
+  $scope.empty = function () {
+      $scope.CustomStyle = {
+          'color' : 'red',
+          'border-style': 'solid',
+          'border-color': 'red'
+      };
+  }
+  $scope.enjoyOrTooMuch = function () {
+      $scope.CustomStyle = {
+        'color' : 'green',
+        'border-style': 'solid',
+        'border-color': 'green'
+      };
+  }
 
 }
 
